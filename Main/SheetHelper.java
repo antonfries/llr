7c9f7bff068c0396b51dd3fe37046813a08462d0
Anton Fries <fries@galileo-webagentur.de>
2020-02-04 19:53:45 +0100

    Bugfix: Initial ist die Sheet-Liste leer

--- Main/SheetHelper.java
+++ Main/SheetHelper.java
@@ -8,6 +8,9 @@ import java.util.Enumeration;
 public class SheetHelper {
 
     public static String getSelectedSheetName(Gui gui) {
+        if (gui.sheetListe == null) {
+            return ""; // Initial ist die Sheet-Liste noch nicht befüllt
+        }
         for (Enumeration<AbstractButton> buttons = gui.sheetListe.getElements(); buttons.hasMoreElements(); ) {
             AbstractButton button = buttons.nextElement();
             if (button.isSelected()) {
